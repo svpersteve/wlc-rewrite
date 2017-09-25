c2d27fa2f62dd164048f2749870d29f45953d579
Steve Brewer <steve.brewer@futurelearn.com>
2017-09-25 17:43:22 +0100

    Create seed file
    This will create the Hackrooms on your local database. Rails just runs the
    commands that are here, and I've used find_or_create_by so it won't duplicate
    existing data if you've already run the seed file before. This allows us to
    add to it later and re-run it.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+@hackroom_one = Hackroom.find_or_create_by!(
+                name: 'Ruby',
+                colour: '#af1401',
+                image: 'https://s3.eu-west-2.amazonaws.com//wlcavatars/uploads/language/image/1/ruby.jpg',
+                description: 'Some details about this language',
+                long_description: 'More details about this hackroom')
+
+@hackroom_two = Hackroom.find_or_create_by!(
+                name: 'iOS',
+                colour: '#ef5138',
+                image: 'https://s3.eu-west-2.amazonaws.com//wlcavatars/uploads/language/image/4/swift.png',
+                description: 'Some details about this language',
+                long_description: 'More details about this hackroom')
+
+@hackroom_three = Hackroom.find_or_create_by!(
+                name: 'Python',
+                colour: '#68d89b',
+                image: 'https://s3.eu-west-2.amazonaws.com//wlcavatars/uploads/language/image/2/python.jpg',
+                description: 'Some details about this language',
+                long_description: 'More details about this hackroom')
+
+@hackroom_four = Hackroom.find_or_create_by!(
+                name: 'Front End',
+                colour: '#f2d50a',
+                image: 'https://s3.eu-west-2.amazonaws.com//wlcavatars/uploads/language/image/5/js.png',
+                description: 'Some details about this language',
+                long_description: 'More details about this hackroom')
+
+HackroomLeader.find_or_create_by!(member: Member.first, hackroom: @hackroom_one)

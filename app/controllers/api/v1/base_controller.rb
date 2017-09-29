2aa350ac33a3c9d4f52322a69d898860832ddee5
Steve Brewer <steve.brewer@futurelearn.com>
2017-09-29 19:10:38 +0100

    Add authentication methods to API controller
    `request_authenticated?` will check if the request URL contains the WLC API
    key and return true if it does, false if not.
    
    You can use this to run an if statement to check if a request is authorized or
    not.
    
    `respond_404_unauthorized` will return an unauthorized response if you'd like
    to deny access to an endpoint that hasn't been sent with the API key.

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -9,4 +9,14 @@ class Api::V1::BaseController < ActionController::API
   def base_api_url
     "http://localhost:3000/api/v1"
   end
+
+  def request_authenticated?
+    request.env['QUERY_STRING'].include? "key=#{ENV['WLC_API_KEY']}"
+  end
+
+  def respond_401_unauthorized
+    render status: 401, json: {
+      message: "You need to include your WLC API key to do this."
+    }
+  end
 end

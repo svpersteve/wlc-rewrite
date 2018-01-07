ed1af47dcdb82dc1e72c20b5b42b02738eddfcbc
Steve Brewer <steve.brewer@futurelearn.com>
2018-01-07 18:14:57 +0000

    Use Meetup model on home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,6 @@
 class PagesController < ApplicationController
   def home
-    get_meetups
-  end
-
-  private
-
-  def get_meetups
-    request = RestClient.get "https://api.meetup.com/West-London-Coders/events?photo-host=public&page=1&sig_id=202775078&sig=7106b5f894f37222f896b703e3a9c6e95f5a65a4"
-    @next_meetup = ActiveSupport::JSON.decode(request).first
+    @upcoming_meetups = Api::Meetup.upcoming(20)
+    @next_meetup = @upcoming_meetups.first unless @upcoming_meetups.empty?
   end
 end

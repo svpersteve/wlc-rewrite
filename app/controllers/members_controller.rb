15f20a02a061fa9a1655a0800a1a01c7d9e3f8cd
Steve Brewer <steve.brewer@futurelearn.com>
2018-01-07 21:45:20 +0000

    Find member's meetup profile for more info

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,11 +1,16 @@
 class MembersController < ApplicationController
   def show
     find_member
+    find_meetup_account
   end
 
   private
 
   def find_member
     @member = Api::Member.find(params[:id])
+  end
+
+  def find_meetup_account
+    @meetup_profile = Api::MeetupProfile.find(params[:id])
   end
 end
